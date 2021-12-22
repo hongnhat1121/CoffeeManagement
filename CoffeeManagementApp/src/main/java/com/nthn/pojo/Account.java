03e100c5586bcc34e2f59b3b74c30ec1736b880c
hongnhat1121 <1951052145nhat@ou.edu.vn>
2021-12-22 15:26:38 +0700

    Init Project

---
+++ CoffeeManagementApp/src/main/java/com/nthn/pojo/Account.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.nthn.pojo;
+
+/**
+ *
+ * @author HONGNHAT
+ */
+public class Account {
+    private String username;
+    private String password;
+}
